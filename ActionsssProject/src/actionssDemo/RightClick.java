8f5233469dcb09b9997a51b2364a9fec2358d3d7
admin <admin@abc>
2019-04-23 12:58:58 -0700

    done

---
+++ ActionsssProject/src/actionssDemo/RightClick.java
@@ -0,0 +1,32 @@
+package actionssDemo;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class RightClick {
+	static
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) throws AWTException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.get("http://localhost/login.do");
+		WebElement linkText = driver.findElement(By.xpath("//a[text()='actiTIME Inc.']"));
+		
+		Actions act=new Actions(driver);
+		act.contextClick(linkText).perform();
+		
+		Robot r=new Robot();
+		r.keyPress(KeyEvent.VK_W);
+	}
+
+}
